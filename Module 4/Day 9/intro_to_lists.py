b7045ccf7d45bd0f7e9eac76012d81e0232f0bf8
Michael Cimolini <mcimolini@nait.ca>
2026-01-29 11:16:48 -0700

    Demo Day 9 - A03

--- Module 4/Day 9/intro_to_lists.py
+++ Module 4/Day 9/intro_to_lists.py
@@ -1,38 +1,31 @@
-groceries = ["lettuce", "tomatoes", "bread", "milk", "chicken", "apples"]
+groceries = ["letuce", "tomato", "bread", "milk", "cheese", "apples"]
 
-print("Our grocery list is: ")
+print("Grocery list: ")
 print(groceries)
 
-# List access starts at 0
-# Print an item at an index
-print("First item is: ")
+# Access an item at an index (0 based index)
+print("The 3rd item on our grocery list is: ")
+print(groceries[2])
+print("The 1st item on our grocery list is: ")
 print(groceries[0])
 
-# List access in reverse starts at -1
-print("Last item is: ")
-print(groceries[-1])
+# Modifying list items
+groceries[0] = "romaine lettuce" # changes our first item
 
-# IndexErrors are thrown if we try to access an index that doesn't exist.
-# print(groceries[42]) #would throw an IndexError
-
-# We can change items at indexes
-groceries[0] = "romaine lettuce"
+print("The 1st item on our grocery list is: ")
+print(groceries[0])
+print("Grocery list: ")
 print(groceries)
 
-# We can slice lists with [start:end] start is inclusive, end is exclusive
-print("First 3 items: ")
-print(groceries[:3])
+# Slicing lists - not a permanent change unless you reassign the output to the variable
+print("The first 3 items are: ")
+print(groceries[0:3]) # 0 could be left out
 
-print("Last 2 items: ")
+print("The last 2 items: ")
 print(groceries[-2:])
-print(groceries[4:6])
+# len() gets the length of my list
+print(groceries[len(groceries) - 2:]) # the same as above, I can use variables as my index
 
-# len() is used to measure our list (count the number of items in it)
-print(f"Our grocery list has {len(groceries)} items.")
-
-print("The last 3 items are: ")
-print(groceries[len(groceries) - 3 : len(groceries)])
-
-# Can replace a hard coded index (0) with a variable
-item = int(input("Select an item: "))
-print(groceries[item])
+item_input = input(f"What item would you like? (0 - {len(groceries) - 1}) ") # len(groceries) - 1 will give us the highest index we can choose before we get an IndexError
+item_index = int(item_input)
+print(f"You have selected {groceries[item_index]}")
